f1f01d8eea51117648998d8c368cd09c628a9049
covel1 <co.velicu@gmail.com>
2018-04-18 22:41:59 +0300

    Include service worker registration inside the file

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,3 +1,32 @@
+/**
+* Register service worker
+*/
+
+if ('serviceWorker' in navigator) {
+	  // Register a service worker hosted at the root of the
+	  // site using the default scope.
+	  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+		console.log('Service worker registration succeeded:', registration);
+	  }).catch(function(error) {
+		console.log('Service worker registration failed:', error);
+	  });
+
+	  // Do a one-off check if there's currently a
+	  // service worker in control.
+	  if (navigator.serviceWorker.controller) {
+		console.log('This page is currently controlled by:', navigator.serviceWorker.controller);
+	  }
+
+	  // Register a handler to detect when a new or
+	  // updated service worker takes control.
+	  navigator.serviceWorker.oncontrollerchange = function() {
+		console.log('This page is now controlled by:', navigator.serviceWorker.controller);
+	  };
+	} else {
+	  console.log('Service workers are not supported.');
+}
+/* Registration end */
+
 let restaurant;
 var map;
 
